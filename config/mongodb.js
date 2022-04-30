3e264745930c88bb77bd61c7cef3ce9560e16ea1
vadivelkt <ext-vadivel@perfios.com>
2022-04-30 11:24:46 +0530

    kjkj

--- config/mongodb.js
+++ config/mongodb.js
@@ -12,7 +12,7 @@ const Connection_Options={
 }
 
 const  Client= async ()=>{
-    await db.connect(`mongodb://${process.env.DB_USR_NAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&retryWrites=false`,Connection_Options,(res)=>{
+     db.connect(`mongodb://${process.env.DB_USR_NAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&retryWrites=false`,Connection_Options).then(res=>{
         console.log(res)
     }).catch(eer=>{
         console.log(eer);
